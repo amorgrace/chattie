373fb3b2d571f0c3819b0539b0217573aa6c99fe
amorgrace <akanforte@gmail.com>
2025-09-15 22:23:53 +0100

    scroltotop

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -1,14 +1,13 @@
-import { useEffect } from 'react'
-import { useLocation } from 'react-router-dom'
+// src/components/ScrollToTop.jsx
+import { useEffect } from "react"
+import { useLocation } from "react-router-dom"
 
-function ScrollToTop() {
+export default function ScrollToTop({ children }) {
   const { pathname } = useLocation()
 
   useEffect(() => {
-    window.scrollTo(0, 0)
+    window.scrollTo({ top: 0, left: 0, behavior: "instant" })
   }, [pathname])
 
-  return null
+  return children || null
 }
-
-export default ScrollToTop
